81bd604875ab8590d0d95b105d0263169b28e505
ali shahabbasi <shahabbasi.ali.98@gmail.com>
2018-06-29 22:44:47 +0430

    functions completed

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-let body = document.querySelector('body');
-let isKeyDown = false;
-let clickPos = {x: 0, y: 0};
+class SwipeHandler{
+    constructor(){
+        this.isKeyDown = false;
+        this.clickPos = {x: 0, y: 0};
+        this.releasePos = {x: 0, y: 0};
+    }
 
-function setKeyDown(isDown){
-    isKeyDown = isDown;
+    setKeyDown(isDown){
+        this.isKeyDown = isDown;
+        if(isDown){
+            this.clickPos = {x: e.clientX, y: e.clientY };
+        }else{
+            this.releasePos = {x: e.clientX, y: e.clientY };
+        }
+    }
+    
+    onkeydown = function(e){
+        setKeyDown(true);
+    };
+    
+    onkeyup = function(e){
+        setKeyDown(false);
+    }
+
 }
 
-body.onkeydown = function(e){
-    setKeyDown(true);
+const swipeHandler = new SwipeHandler();
 
-};
-
-body.onkeyup = function(e){
-    setKeyDown(false);
+body = document.querySelector('body');
+body.onkeydown = (e)=>{
+    swipeHandler.onkeydown(e);
+}
+body.onkeyup = (e)=>{
+    swipeHandler.onkeyup(e);
 }
